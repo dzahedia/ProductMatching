049751779e2994144202e8561bf870ed13671162
david.zahedi <david.zahedi@dataesp.ca>
2016-08-30 13:42:48 -0400

    edited go

--- go.sh
+++ go.sh
@@ -8,7 +8,7 @@ echo "Before we go:"
 echo "This program assumes that Python3, Spark and NumPy are installed in your system."
 echo "If your Spark is tied to a Hadoop Cluster; you need to uncomment/comment two lines in the entrypoint.py"
 echo
-echo "Good to know"
+echo "Good to know:"
 echo "The results file location: results/results.txt"
 echo "The summary file location: results/summary.txt"
 echo "If you want, you can easily change those locations at entrypoint.py"
@@ -17,8 +17,10 @@ echo "Do you want to continue? (y/n)"
 read req
 if [ "$req" == "y" ]
   then
-  echo "On a ThinkPad core i5, it takes 2 minutes!"
-  python3 entrypoint.py
+  echo
+  echo "On a ThinkPad core i5, it takes 2 minutes to run!"
+  echo
+  python3.4 entrypoint.py
 fi
 echo "Done"
 
